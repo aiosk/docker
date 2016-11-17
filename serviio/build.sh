ac769dfe320f7e0ba58919444c6366d32819ed78
Andry <andry.yosua@pinteraktif.com>
2016-11-17 18:21:45 +0700

    update serviio

--- serviio/build.sh
+++ serviio/build.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 set -e
 
-FILE=serviio-1.7-linux.tar.gz
+FILE=serviio-1.7.1.1-linux.tar.gz
 URL="http://download.serviio.org/releases/$FILE"
 
 if [ ! -f "./$FILE" ]; then
 	curl -Lk -o "$FILE" "$URL"
 fi
 
-../replace-for-arm.sh \
-&& cp ../lib.sh ./lib.sh \
-&& docker-compose build
+if [ ! -d "./serviio-1.7.1.1" ]; then
+	tar -zxvf "./$FILE"
+fi
+docker-compose build
