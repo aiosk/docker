39aead300b96ba3f12308d44690445744276fe67
Andry Yosua <andry.yosua@gmail.com>
2016-11-18 18:28:47 +0100

    remove replace for arm script

--- replace-for-arm.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-#find ./ -name Dockerfile -exec sed -i -e 's/^FROM alpine/FROM container4armhf\/armhf-alpine/gI' {} \;
-#find ./ -name Dockerfile -exec sed -i -e 's/^FROM debian/FROM armbuild\/debian/gI' {} \;
