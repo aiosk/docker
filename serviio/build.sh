4dd7597a536be14f2b34471d1fb413486329ddce
Andry Yosua <andry.yosua@gmail.com>
2016-11-21 20:36:08 +0700

    revert

--- serviio/build.sh
+++ serviio/build.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 set -e
 
-FILE=serviio-1.7.1.1-linux.tar.gz
+SERVIIO=serviio-1.7.1.1
+FILE="$SERVIIO-linux.tar.gz"
 URL="http://download.serviio.org/releases/$FILE"
 
 if [ ! -f "./$FILE" ]; then
 	curl -Lk -o "$FILE" "$URL"
 fi
 
-if [ ! -d "./serviio-1.7.1.1" ]; then
+if [ ! -d "./$SERVIIO" ]; then
 	tar -zxvf "./$FILE"
 fi
 docker-compose build
