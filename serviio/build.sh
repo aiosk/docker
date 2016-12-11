2ab1cc662ba592adeb61bfccb0b3430ea0bc9c28
Andry Yosua <andry.yosua@gmail.com>
2016-12-11 19:48:34 +0700

    update serviio 1.8

--- serviio/build.sh
+++ serviio/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -ex
 
-SERVIIO=serviio-1.7.1.1
+SERVIIO=serviio-1.8
 FILE="$SERVIIO-linux.tar.gz"
 URL="http://download.serviio.org/releases/$FILE"
-LIB=library
+LIB="./library"
 
 if [ ! -f "./$FILE" ]; then
 	curl -Lk -o "$FILE" "$URL"
@@ -13,10 +13,10 @@ fi
 if [ ! -d "./$SERVIIO" ]; then
 	tar -zxvf "./$FILE"
 	chmod 755 -R "./$SERVIIO"
-	mkdir -p "./$SERVIIO/log"
-	chmod 777 -R "./$SERVIIO/log"
+	mv "./$SERVIIO/bin/serviio.sh" "./$SERVIIO/bin/serviio.old.sh"
+	cp ./serviio.sh "./$SERVIIO/bin"
 	
-	cp -R "./$SERVIIO/library" "./$LIB"
-	chmod 777 -R "./$LIB"
+	cp -R "./$SERVIIO/library" "$LIB"
+	echo yosuapi | sudo chmod 777 -R "$LIB"
 fi
 docker-compose build
