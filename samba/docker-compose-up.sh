2b5b42a21113d45359c8680aa56f71db7e816e30
Andry Yosua <andry.yosua@gmail.com>
2017-05-17 01:53:33 +0700

    update samba to docker only

--- samba/docker-compose-up.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-
-docker-compose up --remove-orphans -d \
-&& docker-compose logs -f
---
+++ samba/docker-up.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+NAME="aios_samba"
+
+docker rm --force "$NAME"
+
+docker run \
+	--detach \
+	--net=host \
+	--restart=on-failure:3 \
+	--name="$NAME" \
+	--volume=/mnt/data:/shares:rw \
+	aios/samba \
+	sh -c "inetd -d & /usr/sbin/nmbd -FS & /usr/sbin/smbd -FS"
+
+docker logs --follow --tail 20 --timestamps "$NAME"
