a06eb4cc9014dedfe13b5a1d66bc46f30302ab26
Andry Yosua <andry.yosua@gmail.com>
2019-09-04 21:05:58 +0700

    update aria2 and heimdal to prepare for openwrt

--- aria2-ariang/docker-build.sh
+++ aria2-ariang/docker-build.sh
@@ -10,18 +10,18 @@ docker run -d \
   --restart=unless-stopped \
   -p 81:80 \
   -p 6800:6800 \
+  -e PUID=1000 \
+  -e PGID=996 \
   -e ENABLE_AUTH=true \
   -e RPC_SECRET=yosuaaria2 \
   -e ARIA2_USER=andry \
   -e ARIA2_PWD=yosuaaria2 \
   -v /mnt/data/downloads:/data \
+  -v /mnt/data/downloads/aria-filebrowser.db:/app/filebrowser.db \
   -v $PWD/aria2.conf:/root/conf/aria2.conf \
-  -v $PWD/on-complete.sh:/root/conf/on-complete.sh \
   wahyd4/aria2-ui:arm32
 
 #  -p 443:443 \
-#  -e DOMAIN=example.com \
-#  -v /yoursslkeys/:/root/conf/key \
 
 docker start "$NAME"
 docker logs --follow --tail 20 --timestamps "$NAME"
--- heimdall/docker-build.sh
+++ heimdall/docker-build.sh
@@ -10,10 +10,10 @@ docker create \
   --restart=unless-stopped \
   -v $PWD/config:/config \
   -e PGID=996 -e PUID=1000  \
-  -p 80:80 \
-  -p 443:443 \
+  -p 84:80 \
   -e TZ=Asia/Jakarta \
   linuxserver/heimdall
 
+#  -p 443:443 \
 docker start "$NAME"
 docker logs --follow --tail 20 --timestamps "$NAME"
